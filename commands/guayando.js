09e5585f1d0da9c702d1b4739f7d5b2681f67175
David Urbina <davidurbina.dev@gmail.com>
2022-01-18 13:37:33 -0500

    fixed identation

--- commands/guayando.js
+++ commands/guayando.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('guayando')
-    .setDescription('guayando'),
-  async execute(interaction) {
-    await interaction.reply('guayando');
-  },
+    data: new SlashCommandBuilder()
+        .setName('guayando')
+        .setDescription('guayando'),
+    async execute(interaction) {
+        await interaction.reply('guayando');
+    },
 };
--- commands/user.js
+++ commands/user.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('user')
-    .setDescription('Replies with user information.'),
-  async execute(interaction) {
-    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-  },
+    data: new SlashCommandBuilder()
+        .setName('user')
+        .setDescription('Replies with user information.'),
+    async execute(interaction) {
+        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+    },
 };
