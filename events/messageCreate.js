13ade664d9c848277e611f5141c356b3504b66d7
David Urbina <davidurbina.dev@gmail.com>
2022-01-18 16:07:59 -0500

    added proof of concept autoreplies

---
+++ events/messageCreate.js
@@ -0,0 +1,43 @@
+// TODO: fix logger crashes
+// const logger = require('../logger');
+
+const reply = (message, question, answer) => {
+    message.reply(answer)
+        .then(() => console.log(`Replied to ${question}`))
+        .catch(console.error);
+};
+
+module.exports = {
+    name: 'messageCreate',
+    execute(message) {
+        if (message.author.bot) return;
+
+        const content = message.content.toLowerCase();
+
+        // TODO: refactor this into a DBMS
+        if (content.startsWith('guayando')) {
+            reply(message, 'guayando', 'Guayando! turu turuu ruuruu ru tu');
+        }
+        else if (content.startsWith('reconoceme') || content.startsWith('reconozcame') || content.startsWith('acknowledge me')) {
+            reply(message, 'acknowledge me', 'Por favor ve al psicologo');
+        }
+        else if (content.startsWith('epale')) {
+            reply(message, 'epale', 'Epale mi pana, todo fino?');
+        }
+        else if (content.startsWith('aguacate con arroz')) {
+            reply(message, 'aguacate-con-arroz', 'mondongo');
+        }
+        else if (content.startsWith('no te atrevas a ser amargado')) {
+            reply(message, 'new-day', 'Aplaude a tus campeones en pareja y siente el podeeeeer');
+        }
+        else if (content === 'es el') {
+            reply(message, 'don-comedias', 'El legendario súper comediante\n');
+        }
+        else if (content === '\u{1F921}') {
+            reply(message, 'don-comedias-emoji', 'Me llama usted, entonces voy, Don comedia es quien yo soy \u{1F921}');
+        }
+        else if (content === '\u{1F446}' || content === '\u261D') {
+            reply(message, 'el-jefe-tribal', 'Arrodíllense y reconozcan al rey, el número uno, el que pone la comida sobre la mesa, el perrote, el dueño de todos los patios, el primo de la piedra, el poder personificado, la fuerza del escudo, EL TIPO, el que enterró al Undertaker\n\nEL JEFE TRIBAL');
+        }
+    },
+};
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
+const {
+    Client,
+    Collection,
+    Intents,
+} = require('discord.js');
 const logger = require('./logger');
 
 require('dotenv').config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+    ]
+});
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
