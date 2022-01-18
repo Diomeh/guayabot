3c591ed5a4ece48b45245c7ddcb5b4300c700c6f
David Urbina <davidurbina.dev@gmail.com>
2022-01-18 13:29:09 -0500

    added commands

---
+++ commands/echo.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const data = new SlashCommandBuilder()
+  .setName('echo')
+  .setDescription('Replies with your input!')
+  .addStringOption(option =>
+    option.setName('input')
+      .setDescription('The input to echo back')
+      .setRequired(true));
+
+module.exports = {
+  data: data,
+  async execute(interaction) {
+    const input = interaction.options.getString('input');
+    await interaction.reply(input);
+  },
+};
---
+++ commands/guayando.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('guayando')
+    .setDescription('guayando'),
+  async execute(interaction) {
+    await interaction.reply('guayando');
+  },
+};
