4e666253020195eba48ce0017383175952a1afc3
David Urbina <davidurbina.dev@gmail.com>
2022-08-27 18:41:02 -0500

    Created `core` module

---
+++ src/core/index.ts
@@ -0,0 +1,16 @@
+import { Client, ClientOptions, Collection } from 'discord.js';
+import { Command } from '@/types';
+
+export { default as logger } from '@/core/logger';
+export { default as db } from '@/core/database';
+
+export class ExtendedClient extends Client {
+    public commands: Collection<string, Command>;
+    public events: Collection<string, any>;
+
+    public constructor(options: ClientOptions) {
+        super(options);
+        this.commands = new Collection();
+        this.events = new Collection();
+    };    
+};    
