e5a6db102859b235549a0df955f4bb4db2c418b4
David Urbina <davidurbina.dev@gmail.com>
2022-08-28 13:23:22 -0500

    Initialized sequelize instance before calling any model

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,12 +1,21 @@
 import { Client, ClientOptions, Collection } from 'discord.js';
 import { Command, Event } from '@/types';
 
+import { default as logger } from '@/core/logger';
+import { default as db } from '@/core/database';
+import { default as CommandDeployer } from '@/core/deployCommands';
+
+// commands can have an implicit call to models, which in turn requires a sequelize instance
+// this is a workaround to make sure the sequelize instance is available before the commands are imported
+// therefore, we first import (and thus initialize) the sequelize instance, then import the commands
 import * as commands from '@/commands';
 import * as events from '@/events';
 
-export { default as logger } from '@/core/logger';
-export { default as db } from '@/core/database';
-export { default as CommandDeployer } from '@/core/deployCommands';
+export {
+    logger,
+    db,
+    CommandDeployer,
+};
 
 export class ExtendedClient extends Client {
     public commands: Collection<string, Command>;
